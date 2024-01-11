b5f8b29882df33717a2d6d90f53be6da678dfd28
Cristhian Villca Mamani <cristhian.villca@tugerente.com>
2024-01-11 00:17:13 -0400

    clase de 6-10

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # dirección para el administrador.
     path('admin/', admin.site.urls),
+    
+    # dirección de la vista principal -> la que se ve primero.
     path('',views.index, name='principal'),
-    path('saludo/',views.saludo, name='saludo')
+    
+    # dirección de un saludo.
+    path('saludo/',views.saludo, name='saludo'),
+    
+    # dirección de un saludo mejorado ingresando una variable.
+    path('saludoMejorado/<str:nombre>', views.saludoMejorado, name='saludo2'),
+    
+    # dirección para la vista contacto.
+    path('contacto/',views.contacto, name='contacto'),
+    
+    # dirección para la vista sobreNosotros con un parámetro por la URL.
+    path('sobreNosotros/<str:nombre>',views.sobreNosotros, name='sobreNosotros')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+# TODO: retorna una respuesta por defecto.
 def index(request):
     return HttpResponse("Hola Mundo")
 
+# TODO: retorna un saludo.
 def saludo(request):
     return HttpResponse("Hola saludos desde Django")
+
+# TODO: retorna un saludo con un nombre personalizado.
+def saludoMejorado(request, nombre):
+    return HttpResponse(f"Hola {nombre} como estas?")
+
+# TODO: retorna una vista html 
+def contacto(request):
+    return render(request,'contacto.html',{})
+
+# TODO: retorna una vista html con una variables adicional para el contexto.
+def sobreNosotros(request,nombre):
+    lenguajes = ['php','java','c','c#','c++','python','js','go','rust','ruby','kotlin']
+    context = { 'nombre' : nombre, 'lenguajes' : lenguajes }
+    return render(request,'sobreNosotros.html',context)
